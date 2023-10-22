5ba78b4e256bd42ec1130a4b2bccd001fd9ef048
Alexandre Marques Bulcao <ambulcao@gmail.com>
2023-10-22 12:55:45 +0100

    Displaying Directions
    Displaying Directions

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -8,6 +8,7 @@ import {
 } from '@react-google-maps/api'
 //import '../App.css'
 import  PlaceDetail  from './placeDetail';
+import { position } from '@chakra-ui/react';
 
 //import Distance from './distance'
 
@@ -17,9 +18,10 @@ type MapOptions = google.maps.MapOptions
 
 
 export default function LocationMap() {
-  const [office, setOffice] = useState<LatLngLiteral>();
-  const mapRef = useRef<GoogleMap>();
-  const center = useMemo<LatLngLiteral>(() => ({ lat: 43, lng: -80 }), [])
+  const [office, setOffice] = useState<LatLngLiteral>()
+  const [directions, setDirections] = useState<DirectionsResult>()
+  const mapRef = useRef<GoogleMap>()
+  const center = useMemo<LatLngLiteral>(() => ({ lat: 43.45, lng: -80.49 }), [])
   const options = useMemo<MapOptions>(() => ({
     mapId: "13a84d305d69091b",
     disableDefaultUI: true,
@@ -30,6 +32,23 @@ export default function LocationMap() {
   }, []);*/
   const onLoad = useCallback((map) => (mapRef.current = map), [])
   const houses = useMemo(() => generateHouses(center), [center]) 
+  const fetchDirections = (house: LatLngLiteral) => {
+    if(!office) return;
+
+    const service = new google.maps.DirectionsService()
+    service.route(
+      {
+        origin: house,
+        destination: office,
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (result, status) => {
+        if(status === "OK" && result) {
+          setDirections(result)
+        }
+      }
+    )
+  }
   const image = "https://developers.google.com/maps/documentation/javascript/exemples/full/images/beachflag.png";
         
   return <div className='container'>
@@ -50,6 +69,14 @@ export default function LocationMap() {
         onLoad={onLoad}
       >
         
+        {directions && <DirectionsRenderer directions={directions} options={{
+          polylineOptions: {
+            zIndex: 50,
+            strokeColor: "#1976D2",
+            strokeWeight: 5,
+          }
+        }} />}
+
         { office && ( 
           <>
             <Marker 
@@ -57,14 +84,22 @@ export default function LocationMap() {
             />
             
             <MarkerClusterer>
-              {clusterer => houses.map((house) => (
-                <Marker 
-                  key={house.lat} 
-                  position={house} 
-                  clusterer={clusterer}
-                />
-            ))}
+              {(clusterer) => (
+                  <>
+                    {houses.map(house => (
+                      <Marker
+                        key={house.lat}
+                        position={house}
+                        clusterer={clusterer}
+                        onClick={() => {
+                          fetchDirections(house);
+                        }}
+                      />
+                    ))}
+                  </>
+                )}
             </MarkerClusterer>
+
 
             <Circle
               center={office}
