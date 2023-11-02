a3f41a0ad0f911d94917d789b0fabb38e212b1da
Alexandre Marques Bulcao <ambulcao@gmail.com>
2023-11-02 12:43:22 +0000

    Layout tela login
    Layout tela login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import './Login.scss'
-import video from '../../LoginAssets/video.webm'
-import videoWebm from '../../LoginAssets/video.mp4'
+import React from "react";
+import "./Login.scss";
+import { Link } from "react-router-dom";
+import video from "../../LoginAssets/video.webm";
+import videoWebm from "../../LoginAssets/video.mp4";
+import logo from "../../LoginAssets/logo.png";
 
 const Login = () => {
   return (
-    <div className='loginPage flex'>
-      <div className='container flex'>
-
+    <div className="loginPage flex">
+      <div className="container flex">
         <div className="videoDiv">
           <video autoPlay loop>
             <source src={video} type="video/mp4" />
             <source src={videoWebm} type="video/webm" />
             Seu navegador não suporta a reprodução de vídeo.
           </video>
+
+          <div className="textDiv">
+            <h2 className="title">Search your favorite store</h2>
+            <p>A complete catalog</p>
+          </div>
+
+          <div className="footerDiv flex">
+            <span className="text">Don't have an account?</span>
+            <Link to={"/register"}>
+              <button className="btn">Sign Up</button>
+            </Link>
+          </div>
         </div>
 
-        {/*<a href='/register'>To Register</a>
-        <br />
-        <a href='/home'>To Login</a>
-        <br />
-          This is Login Page*/}
+        <div className="formDiv flex">
+          <div className="headerDiv">
+            <img src={logo} alt="Logo Image" />
+            <h3>Welcome Back!</h3>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
