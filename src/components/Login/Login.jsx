c7d5effbda72d88e83f4e8071a16c432b33021e9
Alexandre Marques Bulcao <ambulcao@gmail.com>
2023-11-02 17:19:26 +0000

    Assets Tela Login
    Assets Tela Login e layout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 import { FaUserShield } from 'react-icons/fa';
+import { BsFillShieldLockFill } from 'react-icons/bs';
+import { AiOutlineSwapRight } from 'react-icons/ai';
 import video from "../../LoginAssets/video.webm";
 import videoWebm from "../../LoginAssets/video.mp4";
 import logo from "../../LoginAssets/logo.png";
@@ -42,8 +44,27 @@ const Login = () => {
               <label htmlFor="username">Username</label>
               <div className="input flex">
                 <FaUserShield className='icon'/>
+                <input type="text" id="username" placeholder="Enter Username"/>
               </div>
             </div>
+
+            <div className="inputDiv">
+              <label htmlFor="password">Password</label>
+              <div className="input flex">
+                <BsFillShieldLockFill className='icon'/>
+                <input type="password" id="password" placeholder="Enter Password"/>
+              </div>
+            </div>
+
+            <button type="submit" className="btn flex">
+              <span>Login </span>
+              <AiOutlineSwapRight className='icon' />
+            </button>
+
+            <span className="forgotPassword">
+              Forgot yout password? <a href="">Click Here</a>
+            </span>
+
           </form>
         </div>
       </div>
